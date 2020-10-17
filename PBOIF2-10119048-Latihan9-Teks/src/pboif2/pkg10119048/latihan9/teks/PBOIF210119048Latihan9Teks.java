ad3f58049547fcf9db7cdfc05fb5944a86de38ac
Mochammad Faishal <72900665+fshll666@users.noreply.github.com>
2020-10-17 23:29:55 +0700

    Add files via upload

---
+++ PBOIF2-10119048-Latihan9-Teks/src/pboif2/pkg10119048/latihan9/teks/PBOIF210119048Latihan9Teks.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119048.latihan9.teks;
+
+/**
+ *
+ * @author 
+ * Nama      : Mochammad Faishal
+ * Kelas     : IF2
+ * NIM       : 10119048
+ * Deskripsi : Menampilkan Teks
+ */
+public class PBOIF210119048Latihan9Teks {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        char c;
+        char tab = '\t';
+        char newline = '\n';
+        c = 'A';
+        System.out.println("Hasil" + newline + "char :" + tab + "berisi" + c);
+    }
+    
+}
